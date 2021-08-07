7057bc7a39d7e6cffb600f80c2eab050eb88bfe7
Scott Milliorn <scottmilliorn@gmail.com>
2021-08-06 20:23:10 -0700

    Add a to-do item

--- TodoApi/wwwroot/js/site.js
+++ TodoApi/wwwroot/js/site.js
@@ -123,3 +123,27 @@ function _displayItems(data) {
 
     todos = data;
 }
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
